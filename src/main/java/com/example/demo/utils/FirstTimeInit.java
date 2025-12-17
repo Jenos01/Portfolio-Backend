3efe165cd30d331f4091c725c543b0043b6c9d47
BouAli Med Amin <aminboualiaminbouali@gmail.com>
2025-12-17 17:43:14 +0100

    adding screenshot

--- src/main/java/com/example/demo/utils/FirstTimeInit.java
+++ src/main/java/com/example/demo/utils/FirstTimeInit.java
@@ -36,7 +36,7 @@ public class FirstTimeInit implements CommandLineRunner {
     private final RateRepository rateRepository;
     private final CommentRepository commentRepository;
 
-    private Skills skill1,skill2,skill3,skill4,skill5,skill6;
+    private Skills skill1,skill2,skill3,skill4,skill5,skill6,skill7,skill8,skill9;
     private Project project1,project2,project3,project4,project5,project6;
     private Users user1,user2,user3,user4,user5,user6;
 
@@ -48,10 +48,15 @@ public class FirstTimeInit implements CommandLineRunner {
         if (skillsRepository.count() == 0) {
             skill1 = skillsRepository.save(new Skills(null, "Angular", "https://img.icons8.com/?size=100&id=71257&format=png&color=000000",new ArrayList<>()));
             skill2 = skillsRepository.save(new Skills(null, "Python", "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",new ArrayList<>()));
-            skill3 = skillsRepository.save(new Skills(null, "C", "http://portfolio.test/projects/c_original_logo_icon_146611.svg",new ArrayList<>()));
+            skill3 = skillsRepository.save(new Skills(null, "C", "http://portfolio.test/Skills/icons8-c-96.png",new ArrayList<>()));
             skill4 = skillsRepository.save(new Skills(null, "Power BI", "https://img.icons8.com/?size=100&id=3sGOUDo9nJ4k&format=png&color=000000",new ArrayList<>()));
             skill5 = skillsRepository.save(new Skills(null, "SpringBoot", "https://img.icons8.com/?size=100&id=90519&format=png&color=000000",new ArrayList<>()));
             skill6 = skillsRepository.save(new Skills(null, "SQL", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg",new ArrayList<>()));
+            skill7 = skillsRepository.save(new Skills(null, "PostgreSQL", "http://portfolio.test/Skills/icons8-postgresql-96.png",new ArrayList<>()));
+            skill8 = skillsRepository.save(new Skills(null, "C#", "http://portfolio.test/Skills/C_Sharp.png",new ArrayList<>()));
+            skill9 = skillsRepository.save(new Skills(null, "Unity", "http://portfolio.test/Skills/images.png",new ArrayList<>()));
+            /// Java, HTML, CSS, SCSS, TypeScript, ...
+
         }
     }
 
@@ -62,35 +67,64 @@ public class FirstTimeInit implements CommandLineRunner {
     private void createProjects(){
             if(projectRepository.count() == 0) {
 
-                ProjectSkills projectSkill1 = new ProjectSkills(null, null, skill1);
-                ProjectSkills projectSkill2 = new ProjectSkills(null, null, skill2);
-                ProjectSkills projectSkill3 = new ProjectSkills(null, null, skill3);
-                ProjectSkills projectSkill4 = new ProjectSkills(null, null, skill4);
-                ProjectSkills projectSkill5 = new ProjectSkills(null, null, skill5);
-                ProjectSkills projectSkill6 = new ProjectSkills(null, null, skill6);
-                ProjectSkills projectSkill7 = new ProjectSkills(null, null, skill5);
-                ProjectSkills projectSkill8 = new ProjectSkills(null, null, skill2);
+                ProjectSkills projectSkill1 = new ProjectSkills(null, null, skill1);//angular
+                ProjectSkills projectSkill2 = new ProjectSkills(null, null, skill2);//python
+                ProjectSkills projectSkill3 = new ProjectSkills(null, null, skill3);//C
+                ProjectSkills projectSkill4 = new ProjectSkills(null, null, skill4);//PowerBi
+                ProjectSkills projectSkill5 = new ProjectSkills(null, null, skill5);//Springboot
+                ProjectSkills projectSkill6 = new ProjectSkills(null, null, skill6);//SQL
+                ProjectSkills projectSkill7 = new ProjectSkills(null, null, skill7);//PostgreSQL
+                ProjectSkills projectSkill8 = new ProjectSkills(null, null, skill8);//C#
+                ProjectSkills projectSkill9 = new ProjectSkills(null, null, skill9);//Unity
+                /// ----------------------
+                ProjectSkills projectSkill12 = new ProjectSkills(null, null, skill1);//angular
+                ProjectSkills projectSkill52 = new ProjectSkills(null, null, skill5);//Springboot
+                ProjectSkills projectSkill72 = new ProjectSkills(null, null, skill7);//PostgreSQL
+                /// ----------------------
+                ProjectSkills projectSkill13 = new ProjectSkills(null, null, skill1);//angular
+                ProjectSkills projectSkill53 = new ProjectSkills(null, null, skill5);//Springboot
+                ProjectSkills projectSkill73 = new ProjectSkills(null, null, skill7);//PostgreSQL
+                /// ----------------------
+                ProjectSkills projectSkill44 = new ProjectSkills(null, null, skill4);//PowerBi
+                /// ----------------------
+                ProjectSkills projectSkill45 = new ProjectSkills(null, null, skill4);//PowerBi
 
 
-                project1 = new Project(null, "HR Dashboard", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/hrdashboard","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill1,projectSkill2)));
+                ///************************
+                project1 = new Project(null, "POS System","Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda","https://github.com/Jenos01/POS-System","http://portfolio.test/projects/dash.jpg",new ArrayList<>(List.of(projectSkill1, projectSkill5, projectSkill7)));
                 projectSkill1.setProject(project1);
-                projectSkill2.setProject(project1);
+                projectSkill5.setProject(project1);
+                projectSkill7.setProject(project1);
                 projectRepository.save(project1);
 
-                project2 = new Project(null, "Heart Disease Prediction", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/heardiseaseprediction","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill3,projectSkill4)));
-                projectSkill3.setProject(project2);
+                ///
+                project2 = new Project(null, "Portfolio", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/portfolio","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill12,projectSkill4,projectSkill52,projectSkill72)));
+                projectSkill12.setProject(project2);
                 projectSkill4.setProject(project2);
+                projectSkill52.setProject(project2);
+                projectSkill72.setProject(project2);
                 projectRepository.save(project2);
 
-                project3 = new Project(null, "VR PFE Project", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/VRPfeProject","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill5,projectSkill6)));
-                projectSkill5.setProject(project3);
-                projectSkill6.setProject(project3);
+                ///
+                project3 = new Project(null, "Kitchen And Bedroom Virtual Tour", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/VRPfeProject","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill13,projectSkill53,projectSkill73,projectSkill8,projectSkill9)));
+                projectSkill13.setProject(project3);
+                projectSkill53.setProject(project3);
+                projectSkill73.setProject(project3);
+                projectSkill8.setProject(project3);
+                projectSkill9.setProject(project3);
                 projectRepository.save(project3);
 
-                project4 = new Project(null, "Portfolio", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/portfolio","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill7,projectSkill8)));
-                projectSkill7.setProject(project4);
-                projectSkill8.setProject(project4);
+                ///
+                project4 = new Project(null, "Heart Disease Prediction", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/heardiseaseprediction","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill2,projectSkill44)));
+                projectSkill2.setProject(project4);
+                projectSkill44.setProject(project4);
                 projectRepository.save(project4);
+
+                ///
+                project5 = new Project(null, "HR Dashboard", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ipsa assumenda", "github.com/hrdashboard","http://portfolio.test/projects/dash.jpg", new ArrayList<>(List.of(projectSkill45)));
+                projectSkill45.setProject(project5);
+                projectRepository.save(project5);
+
             }
         }
 
